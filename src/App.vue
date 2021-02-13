a4930fb52ab355745ff59a7db262acf2d2d1b8c1
Paulo <pauloh16.pdr@gmail.com>
2021-02-13 04:58:33 -0300

    Criar componente para header do site

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,43 @@
 <template>
   <div id="app">
+    <HeaderSite />
+
+    <router-view></router-view>
   </div>
 </template>
 
-<style>
+<script>
+  import HeaderSite from "@/components/HeaderSite.vue";
 
+  export default {
+    components: {
+      HeaderSite
+    }
+  }
+</script>
+
+<style>
+  *, *::after, *::before {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: inherit;
+    text-decoration: none;
+    list-style: none;
+    color: inherit;
+  }
+
+  body {
+    font-family: "Avenir", Arial, Helvetica, sans-serif;
+    color: #234;
+  }
+
+  a {
+    font-weight: bold;
+    transition: color 0.3s ease;
+  }
+
+  a.router-link-active, a:hover {
+    color: #4B8;
+  }
 </style>
---
+++ src/components/HeaderSite.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+      <nav>
+            <div class="logo">
+                <router-link to="/">Techno</router-link>
+            </div>
+
+            <ul>
+                <li><router-link to="/cursos">Cursos</router-link></li>
+                <li><router-link to="/contato">Contato</router-link></li>
+            </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderSite'
+    }
+</script>
+
+<style scoped>
+    header {
+        color: #FFF;
+        background-color: #234;
+    }
+
+    nav {
+        max-width: 800px;
+        padding: 20px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .logo a.router-link-active {
+        color: #FFF;
+    }
+
+    ul li {
+        display: inline-block;
+    }
+
+    ul li + li {
+        margin-left: 10px;
+    }
+
+    ul li a {
+        padding: 0 10px;
+    }
+</style>
